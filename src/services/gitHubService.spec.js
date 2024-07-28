a354dda56c57e9b72b9eaf0d5da5355250a4fe97
JhonataOliveiraTeixeira <jonasdeot@gmail.com>
2024-07-28 13:15:16 -0300

    Modification test

--- src/services/gitHubService.spec.js
+++ src/services/gitHubService.spec.js
@@ -2,12 +2,10 @@ import { beforeEach, describe, expect, it, vi } from 'vitest'
 import axios from 'axios'
 import { getUserFromGitHub } from './gitHubService.js'
 
-// Mock do axios
 vi.mock('axios')
 
 describe('GitHub Service', () => {
     beforeEach(() => {
-        // Limpar todos os mocks antes de cada teste
         vi.clearAllMocks()
     })
 
@@ -15,16 +13,12 @@ describe('GitHub Service', () => {
         const username = 'octocat'
         const userData = { login: 'octocat', id: 1, name: 'The Octocat' }
 
-        // Configurar o mock para retornar o objeto de dados do usuário
         axios.get.mockResolvedValueOnce({ data: userData })
 
-        // Chamar a função que está sendo testada
         const user = await getUserFromGitHub(username)
 
-        // Verificar se a resposta é igual aos dados esperados
         expect(user).toEqual(userData)
 
-        // Verificar se o axios.get foi chamado com a URL correta
         expect(axios.get).toHaveBeenCalledWith(`https://api.github.com/users/${username}`)
     })
 
